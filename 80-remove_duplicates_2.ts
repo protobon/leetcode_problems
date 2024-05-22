889474a4fa5629c3d716e1c1e120af73b2140482
ayrton <ayrtoncoelhods@gmail.com>
2024-05-21 21:53:02 -0300

    allows two equal values at most

---
+++ 80-remove_duplicates_2.ts
@@ -0,0 +1,33 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove some 
+duplicates in-place such that each unique element appears at most twice. 
+The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, 
+you must instead have the result be placed in the first part of the array nums.
+More formally, if there are k elements after removing the duplicates,
+then the first k elements of nums should hold the final result. 
+It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying 
+the input array in-place with O(1) extra memory.
+*/
+function removeDuplicates(nums: number[]): number {
+    if (nums.length === 0) return 0;
+    let i = 0;
+    let duplicated = false
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            duplicated = false
+            i++;
+            nums[i] = nums[j];
+        } else if (!duplicated) {
+            duplicated = true
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+};
